0cd9be8d8064293ec61a1557323b953f1cf7d2d8
Samikshya Sanskruti Swain <samikshyasanskrutiswain@gmail.com>
2026-03-19 22:50:15 +0530

    program 10 added

---
+++ Shell Scripting/p10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in {1..5}; do
+	for j in $(seq 1 $(($1-i))); do   #for j in $(seq 1 $((6-i))); do
+	  echo -n "*"
+	done
+	for k in $(seq 1 $i)
+	do
+	echo -n "*"
+	done
+echo ""
+done
